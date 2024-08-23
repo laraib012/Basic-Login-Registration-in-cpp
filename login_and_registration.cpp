da466eae329e5e283268eecdec2694e12b53626f
laraib012 <148374637+laraib012@users.noreply.github.com>
2024-08-23 17:13:31 +0500

    Update login_and_registration.cpp

--- login_and_registration.cpp
+++ login_and_registration.cpp
@@ -1,239 +1,309 @@
-#include <bits/stdc++.h> // Login and Registration System
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <string.h>
+#include<windows.h>
+#include<conio.h>
+#include <chrono>
+#include <iomanip>
+#include <cstdio>
 using namespace std;
-void NewRegister();
-bool validEmail(string);
-bool validfirstname(string);
-bool validlastname(string);
-bool validpassword(string);
-void verificationcode();
-void Login();
-int main()
+
+void gotoRowCol(int rpos, int cpos)
 {
-    // when the user opens the website
-    cout << "Hey.." << endl;
-    cout << "Welcome to our website!" << endl;
-    cout << "Let's begin the journey.." << endl
-         << endl;
-
-    NewRegister();
-
-    return 0;
+    int xpos=cpos, ypos = rpos;
+    COORD scrn;
+    HANDLE hOuput = GetStdHandle(STD_OUTPUT_HANDLE);
+    scrn.X = xpos;
+    scrn.Y = ypos;
+    SetConsoleCursorPosition(hOuput, scrn);
 }
-void NewRegister() // This function asks the user if he is a new user or have already an account
+void sleep(int m)
 {
-
-    string NewRegister, Username, Password, firstname, lastname, email;
-
-    cout << "New register?  \n(Note: if you are a new user type Yes, otherwise type No) \n";
-    
-    cin >> NewRegister; // must be "yes" if he is a new user or "no" if he is not a new user
-    cin.ignore();
-
-    if (NewRegister == "Yes" || NewRegister == "yes" )
+    for(int j=0; j<m*21000; j++)
     {
-        cout << "First name : "; // enter first name
-        cin >> firstname;
-        while (validfirstname(firstname) == false)
-        {
-            cout << "The first name is not valid please try again : " << endl;
-            cin >> firstname;
-        }
-        cout << endl
-             << "Valid first name." << endl
-             << endl;
 
-
-        cout << "Last name : "; // enter last name
-        cin >>lastname ;
-        while (validlastname(lastname) == false)
-        {
-            cout << "The last name is not valid please try again : " << endl;
-            cin >> lastname;
-        }
-        cout << endl
-             << "Valid last name." << endl
-             << endl;
-
-        cout << "Email : "; // enter email
-        cin >> email;
-        while (validEmail(email) == false)
-        {
-            cout << "The email address is not valid please try again : " << endl;
-            cin >> email;
-        }
-        cout << endl
-             << "Valid email address." << endl
-             << endl;
-
-        cout << "Password : \n(Note: your password must be at least one digit,one special character, one uppercase letter, and one lowercase letter) \n"; // enter password
-        cin >> Password;
-        while (validpassword(Password) == false)
-        {
-            cout << "The password is not valid please try again : " << endl;
-            cin >> Password;
-        }
-        cout << endl
-             << "Valid password." << endl
-             << endl;
-
-        cout << "Registration completed! \n" << endl;
-        verificationcode();
-    }
-    else if (NewRegister == "No"  ||NewRegister == "no") // have already an account
-    {
-		string Username, Password;
-    cout << endl    << "     Log in       " << endl<< endl;
-    cout << "Enter your username :" << endl;
-    getline(cin, Username);
-
-    cout << "Enter your password :" << endl;
-    cin >> Password;
-
-    cout << endl
-         << "Login success!" << endl;
     }
 }
-bool validfirstname(string firstname) // function to check the validity of the first name
+/////////////////////////////////////////////////////
+
+class user
 {
-    if (firstname[0] >= 'A' && firstname[0] <= 'Z')
-      return true;
-	  else
-	  return false;
-}
-bool validlastname(string lastname) // function to check the validity of the last name
-{
-    if (lastname[0] >= 'A' && lastname[0] <= 'Z')
-      return true;
-    
-    else 
-  	 return false;
-       
-    
-}
-bool validEmail(string email) // function to check the validity of the email
-{
-    int AT = -1, dot = -1;                                                            // variables to store the position of ( @ )and (dot)
-    int counterforAT = 0, counterforDot = 0;                                          // check if there is more than one (@) or (dot)
-    if ((email[0] >= 'a' && email[0] <= 'z') || (email[0] >= 'A' && email[0] <= 'Z')) // if the email address start with letter(valid )
+private:
+    string name;
+    int password, IDnum;
+public:
+    void set_info(string newname, int newpass,int ID)
     {
-        for (int i = 0; i < email.length(); i++)
+        name=newname;
+        password=newpass;
+        IDnum=ID;
+    }
+    string getName()
+    {
+        return name;
+    }
+    int getPassword()
+    {
+        return password;
+    }
+    int getIDnum()
+    {
+        return IDnum;
+    }
+};
+
+class signup
+{
+public:
+    user *ptr[2];
+    int new_id;
+    int password;
+    int count=0;
+    string name;
+    int pass, AccountNum,IDnum;
+
+    int signup_info()
+    {
+        ptr[count]=new user;
+        gotoRowCol(15,30);
+        cout<<"Enter ID Number: "<<endl;
+        gotoRowCol(16,30);
+        cin>>IDnum;
+        gotoRowCol(17,30);
+        cout<<"Enter name: "<<endl;
+        gotoRowCol(18,30);
+        cin>>name;
+        gotoRowCol(19,30);
+        cout<<"Enter password: "<<endl;
+        gotoRowCol(20,30);
+        cin>>pass;
+        gotoRowCol(21,30);
+        //cout<<"enter Account Number: "<<endl;
+        //gotoRowCol(22,30);
+        //cin>>AccountNum;
+        ptr[count]->set_info(name,pass,IDnum);
+        count=count+1;
+        return IDnum;
+    }
+    int login_info()
+    {
+        string aaaa = "Login__Successful__WellCome_:)";
+        int counter = 0;
+        gotoRowCol(15,30);
+        cout<<"Enter your ID Number: "<<endl;
+        gotoRowCol(16,30);
+        cin>>new_id;
+        gotoRowCol(17,30);
+        cout<<"Enter your PassWord: "<<endl;
+        gotoRowCol(18,30);
+        cin>>password;
+
+        ifstream openforLogin("File_handling.txt");
+
+        while (openforLogin >> IDnum >> name >> pass )
         {
-            if (email[i] == '@') // If the character is '@'
+            if (IDnum == new_id && pass == password)
             {
-                AT = i;
-                ++counterforAT;
+                system("cls");
+                  for(int i=0;i<65;i++)
+                    {   
+                        gotoRowCol(3,10+i);
+                        cout << char(219);
+                    }
+                  for(int i=0;i<30;i++)
+                    {
+                        gotoRowCol(3+i,10);
+                        cout << char(219);
+                    }
+                  for(int i=0;i<65;i++)
+                    {
+                        gotoRowCol(33,10+i);
+                        cout << char(219);
+                    }
+                   for(int i=0;i<=30;i++)
+                    {
+                        gotoRowCol(3+i,75);
+                        cout << char(219);
+                    }
+                gotoRowCol(13,15);
+                //////////////////////////////////////
+                for(int i=0;i<30;i++)
+                {
+                    cout << aaaa[i];
+                    sleep(3500);
+                }
+                gotoRowCol(15,15);
+                cout << "--ACCOUNT INFORMATION--"<<endl;
+                gotoRowCol(16,15);
+                cout << "Name: " << name << endl;
+                gotoRowCol(17,15);
+                cout <<"ID: "<< IDnum;
+                gotoRowCol(35, 2);
+                cout << "  ";
+                openforLogin.close();
+                counter++;
+                break;
             }
-
-            else if (email[i] == '.') // If character is '.'
-            {
-                dot = i;
-                ++counterforDot;
-            }
+    
         }
-        if (AT == -1 || dot == -1) // If (@) or (dot)is not present
-
-            return false;
-
-        if (AT > dot) // If (dot) is present before(@)
-
-            return false;
-
-        if (counterforDot > 1 || counterforAT > 1)
-            return false;
-
-        return !(dot >= (email.length() - 1));
-    }
-    else if (email[0] >= '0' && email[0] <= '9') // if the email address start with number (not valid )
-    {
-        return false;
-    }
-    else // if the email address start with other symbols (not valid)
-    {
-        return false;
-    }
-}
-bool validpassword(string password) // function to check the validity of the password
-{
-    int digit = 0, uppercase = 0, lowercase = 0, specialchar = 0; // counter to find if password contain at least one digit,one special character, one uppercase letter, and one lowercase letter
-    if (password.length() >= 8 && password.length() <= 15)
-    {
-
-        if (password.find(" ") == -1)
+        if(counter==0)
         {
-            for (int i = 0; i < password.length(); i++)
+           system("cls");
+                  for(int i=0;i<65;i++)
+                    {   
+                        gotoRowCol(3,10+i);
+                        cout << char(219);
+                    }
+                  for(int i=0;i<30;i++)
+                    {
+                        gotoRowCol(3+i,10);
+                        cout << char(219);
+                    }
+                  for(int i=0;i<65;i++)
+                    {
+                        gotoRowCol(33,10+i);
+                        cout << char(219);
+                    }
+                   for(int i=0;i<=30;i++)
+                    {
+                        gotoRowCol(3+i,75);
+                        cout << char(219);
+                    }
+            gotoRowCol(14,30);
+            cout << "Wrong ID or Password Try Again..!" << endl;
+            login_info();
+        }
+        return IDnum;
+    }
+    void save()
+    {
+        ofstream hout("File_handling.txt",ios::app);
+        if(hout.is_open())
+        {
+            for(int i=0; i<count; i++)
             {
-                if (password[i] >= '0' && password[i] <= '9')
-                {
-                    ++digit;
-                }
-                else if (password[i] >= 'a' && password[i] <= 'z')
-                {
-                    ++lowercase;
-                }
-                else if (password[i] >= 'A' && password[i] <= 'Z')
-                {
-                    ++uppercase;
-                }
-                else if (password[i] == '@' || password[i] == '#' || password[i] == '_')
-                {
-                    ++specialchar;
-                }
+                hout<<ptr[i]->getIDnum()<<" ";
+                hout<<ptr[i]->getName()<<" ";
+                hout<<ptr[i]->getPassword()<<" ";
             }
+            hout<<endl;
+        }
+        hout.close();
+    }
 
-            if (digit == 0 || uppercase == 0 || lowercase == 0 || specialchar == 0)
+    void askUser()
+    {
+        int option;
+          for(int i=0;i<65;i++)
+        {
+            gotoRowCol(3,10+i);
+            cout << char(219);
+        }
+         for(int i=0;i<30;i++)
+        {
+            gotoRowCol(3+i,10);
+            cout << char(219);
+        }
+         for(int i=0;i<65;i++)
+        {
+            gotoRowCol(33,10+i);
+            cout << char(219);
+        }
+        for(int i=0;i<=30;i++)
+        {
+            gotoRowCol(3+i,75);
+            cout << char(219);
+        }
+
+        while(true)
+        {
+             gotoRowCol(10,16);
+            cout << "____WELCOME TO OUR LOGIN AND REGISTRATION SYSTEM_____ :) ";
+            gotoRowCol(15,15);
+            cout << "Enter 1 to SignUp..." << endl;
+            gotoRowCol(16,15);
+            cout << "Enter 2 to LogIn...";
+            //gotoRowCol(16,30);
+            cin >> option;
+                system("cls");
+                  for(int i=0;i<65;i++)
+                    {   
+                        gotoRowCol(3,10+i);
+                        cout << char(219);
+                    }
+                  for(int i=0;i<30;i++)
+                    {
+                        gotoRowCol(3+i,10);
+                        cout << char(219);
+                    }
+                  for(int i=0;i<65;i++)
+                    {
+                        gotoRowCol(33,10+i);
+                        cout << char(219);
+                    }
+                   for(int i=0;i<=30;i++)
+                    {
+                        gotoRowCol(3+i,75);
+                        cout << char(219);
+                    }
+            if(option==1)
             {
-                return false;
+                gotoRowCol(15,30);
+                signup_info();
+                save();
+                system("cls");
+                  for(int i=0;i<65;i++)
+                    {   
+                        gotoRowCol(3,10+i);
+                        cout << char(219);
+                    }
+                  for(int i=0;i<30;i++)
+                    {
+                        gotoRowCol(3+i,10);
+                        cout << char(219);
+                    }
+                  for(int i=0;i<65;i++)
+                    {
+                        gotoRowCol(33,10+i);
+                        cout << char(219);
+                    }
+                   for(int i=0;i<=30;i++)
+                    {
+                        gotoRowCol(3+i,75);
+                        cout << char(219);
+                    }
+                string aaaa = "Registration__Successful__:)";
+                int counter = 0;
+                gotoRowCol(18,16);
+                for(int i=0;i<30;i++)
+                {
+                    cout << aaaa[i];
+                    sleep(3500);
+                }
+                gotoRowCol(35,2);
+                cout << "  ";
+                break;
+            }
+            else if(option==2)
+            {
+                gotoRowCol(15,30);
+                login_info();
+                break;
             }
             else
             {
-                return true;
+                gotoRowCol(1,0);
+                cout << " ";
             }
         }
-        else if (password.find(" ") != -1)
-        {
-
-            return false;
-        }
     }
-
-    cout << "Note : your password length less than 8 characters or more than 15 characters." << endl;
-    return false;
-}
-void verificationcode() // verifying account
+};
+int main()
 {
-    int code ;
+    signup su;
+    su.askUser();
+    return 0;
 
-    cout << "We have sent a verification code to your email to confirm your account." << endl
-         << "Please check your email." << endl;
-
-  
-     cout<<"Email message: Your verification code is  ";
-    srand(time(0));
-
-	for (int i = 0; i <= 3; i++)
-	{
-		cout << rand() % 10 ;
-	}
-    cout<<"\n";
-
-    cout<<"Enter the verification code here : \n";
-	cin>>code;
-
-    cout << "Your account has been verified." << endl;
-    Login();
-}
-void Login()
-{
-    string Username, Password;
-
-    cin.ignore();
-    cout << endl    << "     Log in       " << endl<< endl;
-    cout << "Enter your username :" << endl;
-    getline(cin, Username);
-
-    cout << "Enter your password :" << endl;
-    cin >> Password;
-
-    cout << endl
-         << "Login success!" << endl;
 }
